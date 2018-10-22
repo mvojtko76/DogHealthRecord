34c53a973b32b567bec4a8624832282b9d310503
Melissa Vojtko <mvojtko@student.fullsail.edu>
2018-10-21 20:23:49 -0700

    Add existing project

---
+++ DogHealthRecord/DogHealthRecord/Owner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogHealthRecord
+{
+    class Owner
+    {
+        //Setting up members variable for 
+        string mOwnerFirstName;
+        string mOwnerLastName;
+
+        public static string PrintOwnerScreen(string mOwnerFirstName,string mOwnerLastName)
+        {
+            Console.WriteLine("Hello " + mOwnerFirstName + " " + mOwnerLastName);
+
+        }
+    }
+}
---
+++ DogHealthRecord/DogHealthRecord/Vet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogHealthRecord
+{
+    class Vet
+    {
+        //Setting up members variable for Ve
+        string mVetBusinessName;
+        string mVetName;
+        string mVetAddress;
+        string mVetCity;
+        string mVetState;
+        double mZipCode;
+        string mVetPhone;
+    }
+}
