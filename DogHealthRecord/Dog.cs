7f023609813f27e0e0eac1ec34a729a9b57c10c9
Melissa Vojtko <mvojtko@student.fullsail.edu>
2018-10-29 16:10:19 -0700

    Update files

--- DogHealthRecord/Dog.cs
+++ DogHealthRecord/Dog.cs
@@ -8,5 +8,42 @@ namespace DogHealthRecord
 {
     class Dog
     {
+        string mDog;
+        int mDogAge;
+        string mDogBreed1;
+        string mDogBreed2;
+
+        //Create the constructor function
+        public Dog (string _dog, int _DogAge,string _DogBreed1,string _DogBreed2)
+        {
+            //Use the incomming parameters to inialize our original member variables
+            mDog = _dog;
+            mDogAge = _DogAge;
+            mDogBreed1 = _DogBreed1;
+            mDogBreed2 = _DogBreed2;
+        }
+
+        //Getters
+        public string GetDog()
+        {
+            return mDog;
+
+        }
+
+        public int GetDogAge()
+        {
+            return mDogAge;
+        
+        }
+
+        public string GetDogBreed1()
+        {
+            return mDogBreed1;
+        }
+
+        public string GetDogBreed2()
+        {
+            return mDogBreed2;
+        }
     }
 }
